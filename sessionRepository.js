8011eb17ea972bcbdb7585f546f2ad4ac59c0ccb
madhusudan gohil <madhusudan.gohil@incontact.com>
2017-12-13 13:35:03 -0500

    timeout increased

--- sessionRepository.js
+++ sessionRepository.js
@@ -9,7 +9,7 @@ class SessionRepository {
                     resolve({id: 100, name: 'Client1'});
                 else
                     reject('not found');
-            }, 5000);
+            }, 1000);
         });
         return promise;        
     }
