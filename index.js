6bde462a962a9f1b7ba9b6718be1a501cdcc8e22
madhusudan gohil <madhusudan.gohil@incontact.com>
2018-01-02 11:41:30 -0500

    added index

---
+++ index.js
@@ -0,0 +1,11 @@
+const sms = new(require('./sms'))();
+const SessionRepo = require('./sessionRepository');
+
+
+module.exports.handler = (event, context, callback) => {
+     return sms.handleSession("3097501417")
+                        .then(r => {
+                            console.log(r);
+                            callback(null, r.id);
+                        });
+}
